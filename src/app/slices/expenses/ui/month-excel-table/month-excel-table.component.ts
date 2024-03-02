df0801a875a2300f547277beb8381c2e2205352b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-03-01 21:09:43 -0300

    feat:cell table reactive map

--- src/app/slices/expenses/ui/month-excel-table/month-excel-table.component.ts
+++ src/app/slices/expenses/ui/month-excel-table/month-excel-table.component.ts
@@ -4,19 +4,22 @@ import {
   computed,
   input,
 } from '@angular/core';
-import { ExpenseValue, ExpensesInTheMonth } from '../../../../shared/types/interfaces/month-expenses-in-the-month';
+import {
+  ExpenseValue,
+  ExpensesInTheMonth,
+} from '../../../../shared/types/interfaces/month-expenses-in-the-month';
 import { MatTableModule } from '@angular/material/table';
 import { getAllDaysOfMonth } from '../../utils/functions/function-return-array-days';
 import { CommonModule } from '@angular/common';
-import { CelTableComponent } from "../cel-table/cel-table.component";
+import { CelTableComponent } from '../cel-table/cel-table.component';
 
 @Component({
-    selector: 'app-month-excel-table',
-    standalone: true,
-    templateUrl: './month-excel-table.component.html',
-    styleUrl: './month-excel-table.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatTableModule, CommonModule, CelTableComponent]
+  selector: 'app-month-excel-table',
+  standalone: true,
+  templateUrl: './month-excel-table.component.html',
+  styleUrl: './month-excel-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatTableModule, CommonModule, CelTableComponent],
 })
 export class MonthExcelTableComponent {
   table = input.required<ExpensesInTheMonth>();
@@ -28,18 +31,25 @@ export class MonthExcelTableComponent {
     columns.push('total');
     return columns;
   });
-  signalMappedValues=computed<Map<string,Map<string,ExpenseValue[]>>>(()=>
-    {
-      const mappedValues:Map<string,Map<string,ExpenseValue[]>>=new Map();
-      const tableValue=this.table().expensesMonth;
+  signalMappedValues = computed<Map<string, ExpenseValue>>(() => {
+    const mappedValues: Map<string, ExpenseValue> = new Map();
+    const tableValue = this.table().expensesMonth;
+    tableValue.forEach(expense => {
+      const expenseId = `${expense.expenseId}`;
+      expense.expensesValues.forEach(expenseValue => {
+        const modalityId = `${expenseValue.modalityId}`;
+        const dateExpense = `day-${expenseValue.date.getDay()}`;
+        const keyConcatened = expenseId + '-' + modalityId + '-' + dateExpense;
 
-
-
-      return mappedValues;
-    }
-  )
-
-
-
-
+        if (!mappedValues.has(keyConcatened)) {
+          mappedValues.set(keyConcatened, expenseValue);
+        }
+      });
+    });
+    return mappedValues;
+  });
+  getCellValue(elementId: string, modalityId: string, item: string): any {
+    const key = `${elementId}-${modalityId}-${item}`;
+    return this.signalMappedValues().get(key)?.value;
+  }
 }
