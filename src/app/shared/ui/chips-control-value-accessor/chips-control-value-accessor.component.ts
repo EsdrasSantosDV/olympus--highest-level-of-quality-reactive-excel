d9282a8ae35cfcc59f299aa212ea641fdaf91b5f
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-09-21 23:49:46 -0300

    feat:maicao teste

--- src/app/shared/ui/chips-control-value-accessor/chips-control-value-accessor.component.ts
+++ src/app/shared/ui/chips-control-value-accessor/chips-control-value-accessor.component.ts
@@ -1,11 +1,19 @@
-import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, forwardRef, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chips-control-value-accessor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatAutocompleteModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatOptionModule],
   templateUrl: './chips-control-value-accessor.component.html',
   styleUrl: './chips-control-value-accessor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,13 +26,60 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ],
 })
 export class ChipsControlValueAccessorComponent implements ControlValueAccessor {
-  registerOnChange(fn: any): void {
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  dataSIG = input.required<any[]>();
+  titleSIG = input.required<string>();
+  placeholderSIG = input.required<string>();
+  keySIG = input.required<string>();
+  selectedSIG = signal<any[]>([]);
+  inputValueSIG = signal<string>('');
+  inputValuePerformedSIG = toSignal(toObservable(this.inputValueSIG).pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+  ), {
+    initialValue: '',
+  });
+
+  optionsFilteredSIG =
+    computed(() => {
+      const valueInput = this.inputValuePerformedSIG();
+      const allOptions = this.dataSIG();
+      const keyOption = this.keySIG();
+      return allOptions.filter((value) => {
+          return value[keyOption].toLowerCase().includes(valueInput.toLowerCase());
+        },
+      );
+    });
+
+
+  onChanged!: (value: any) => void;
+  onTouched!: () => void;
+
+
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChanged = fn;
   }
 
-  registerOnTouched(fn: any): void {
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
   }
 
-  writeValue(obj: any): void {
+  writeValue(optionsSelected: any[]): void {
+    this.selectedSIG.set(optionsSelected);
   }
 
+  removeChip(optionChip: any) {
+    console.log('removendo a caceta');
+  }
+
+
+  addChip($event: MatChipInputEvent) {
+    console.log('esdras esteve aqui 1');
+  }
+
+  selectedOption($event: MatAutocompleteSelectedEvent) {
+    console.log('esdras esteve aqui SELECTED');
+    this.inputValueSIG.set('');
+    this.selectedSIG.update((value) => [...value, $event.option.value]);
+  }
 }
--- src/app/slices/forms-study-tech/features/componente-de-explicacao/componente-de-explicacao.component.ts
+++ src/app/slices/forms-study-tech/features/componente-de-explicacao/componente-de-explicacao.component.ts
@@ -1,81 +1,47 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ValidationUniqueComponent } from '../../ui/validation-unique/validation-unique.component';
-import { FormArrayImpostoComponent } from '../../ui/form-array-imposto/form-array-imposto.component';
-import { FormRecordComponent } from '../../ui/form-record/form-record.component';
+import { ChipsComponent } from '../../../../shared/ui/chips/chips.component';
 import {
-  distinctUntilChanged,
-  filter,
-  map,
-  of,
-  scan,
-  take,
-  takeLast,
-  tap,
-} from 'rxjs';
+  ChipsControlValueAccessorComponent,
+} from '../../../../shared/ui/chips-control-value-accessor/chips-control-value-accessor.component';
+import { PokemonService } from '../../../../core/services/pokemon.service';
 
 @Component({
   selector: 'app-componente-de-explicacao',
   standalone: true,
   imports: [
     FormsModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
     CommonModule,
-    ValidationUniqueComponent,
-    FormArrayImpostoComponent,
-    FormRecordComponent,
+    MatChipsModule,
+    ChipsComponent,
+    ChipsControlValueAccessorComponent,
   ],
   templateUrl: './componente-de-explicacao.component.html',
   styleUrl: './componente-de-explicacao.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComponenteDeExplicacaoComponent {
-  competitor: Competitor = {
-    name: '',
-    belt: '',
-    weightClass: '',
-    competitionDate: null,
-    address: {
-      street: '',
-      city: '',
-      state: '',
-    },
-  };
+  formBuilder = inject(FormBuilder);
+  pokemonService = inject(PokemonService);
 
-  constructor() {
-    // Observable que emite uma sequência de números
-    const numbers$ = of(1, 1, 2, 3, 4, 4, 5, 6, 7);
+  //REACTIVE
+  form = this.formBuilder.group({
+    pokemons: this.formBuilder.control([]),
+  });
 
-    // Usamos a função pipe() para aplicar vários operadores
-    numbers$
-      .pipe(
-        // Remove valores consecutivos duplicados
-        distinctUntilChanged(),
-        // Filtra os valores menores ou iguais a 5
-        filter(value => value <= 5),
-        // Multiplica os valores restantes por 10
-        map(value => value * 10),
-        // Limita a 3 primeiros valores do fluxo
-        take(3),
-        // Soma os valores ao longo do tempo
-        scan((acc, value) => acc + value, 0),
-        // Aplica uma ação colateral (log) em cada valor emitido
-        tap(value => console.log('Valor atual após scan:', value)),
-        // Pega apenas o último valor emitido
-        takeLast(1)
-      )
-      .subscribe(result => console.log('Resultado final:', result));
-  }
-}
-export interface Competitor {
-  name: string;
-  belt: string;
-  weightClass: string;
-  competitionDate: Date | null;
-  address: Address;
-}
-export interface Address {
-  street: string;
-  city: string;
-  state: string;
+  pokemons$ = this.pokemonService.getPokemons();
+
+  //TEMPLATE
+  pokemonsTemplateDriven = signal([]);
+
 }
